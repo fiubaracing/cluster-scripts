a437202ca709dbe5a7f0ab3d96bdf71587118a27
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2026-07-19 14:12:45 -0300

    feat: logs

--- install.sh
+++ install.sh
@@ -19,20 +19,37 @@ fi
 UTILS="$THIS_SCRIPT_DIR/utils"
 export INSTALLER_DIR=$THIS_SCRIPT_DIR
 
+mkdir -p $INSTALLER_DIR/logs
+
 # Ensure git considers this directory safe to enable git pulls
 git config --global --add safe.directory $INSTALLER_DIR
 bash $UTILS/update-menu.sh
 
-cp .env /home/admin/variables.config && bash install_script.sh
+cp .env /home/admin/variables.config
+echo "Installing cluster main configuration"
+bash install_script.sh 2>&1 $INSTALLER_DIR/logs/install.log
 
 # Install SLURM configurations
 if [ -n "${RESEND_API_KEY}" ] && [ -n "${RESEND_VERIFIED_DOMAIN}" ]; then
-    bash $UTILS/setup/slurm/email/install.sh
+    echo "Installing SLURM email notification script"
+    bash $UTILS/setup/slurm/email/install.sh 2>&1 $INSTALLER_DIR/logs/slurm_email_install.log
 fi
-bash $UTILS/setup/slurm/power/install.sh
+
+echo "Installing SLURM power management scripts"
+bash $UTILS/setup/slurm/power/install.sh 2>&1 $INSTALLER_DIR/logs/slurm_power_install.log
+
 # Install IPMI configurations
-bash $UTILS/setup/ipmi/install.sh
+echo "Installing IPMI configurations"
+bash $UTILS/setup/ipmi/install.sh 2>&1 $INSTALLER_DIR/logs/ipmi_install.log
+
 # Install system configurations
-bash $UTILS/setup/system/cloudflared/install.sh
-bash $UTILS/setup/system/path/install.sh
-bash $UTILS/setup/system/disk/install.sh
+echo "Installing system configurations"
+
+echo "Installing cloudflared"
+bash $UTILS/setup/system/cloudflared/install.sh 2>&1 $INSTALLER_DIR/logs/cloudflared_install.log
+
+echo "Installing path configurations"
+bash $UTILS/setup/system/path/install.sh 2>&1 $INSTALLER_DIR/logs/path_install.log
+
+echo "Installing disk configurations"
+bash $UTILS/setup/system/disk/install.sh 2>&1 $INSTALLER_DIR/logs/disk_install.log
